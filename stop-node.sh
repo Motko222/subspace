50e03f0ded492d7d0d30a1f4b6f23569cce36818
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-15 16:40:18 +0200

    Update stop-node.sh

--- stop-node.sh
+++ stop-node.sh
@@ -2,7 +2,7 @@
 
 echo "Running nodes:"
 ps aux | grep subspace-node | grep -v grep | awk 'match($0, /subspace[0-9]|subspace[0-9][0-9]/) {print $2" "substr($0, RSTART, RLENGTH)}'
-read -p "Node? " node
-process=$(ps aux | grep subspace-node | grep -v grep | grep "subspace$node " | awk '{print $2}')
+read -p "Node? " id
+process=$(ps aux | grep subspace-node | grep -v grep | grep "subspace$id " | awk '{print $2}')
 echo "Killing process $process..."
 kill $process
