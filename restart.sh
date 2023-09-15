a9f899081e7f06a9dfb6f2e7ddad2e5829286ff5
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-15 21:57:23 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -3,7 +3,7 @@
 if [ -z $1 ]
   then 
     echo "Running nodes:"
-    ps aux | grep subspace-node | grep -v grep | awk 'match($0, /subspace[0-9]|subspace[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}'
+    ps aux | grep subspace-node | grep -v grep | awk 'match($0, /subspace[0-9]|subspace[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}' | sed 's/subspace//g'
     echo "------------------------"
     read -p "Node?  " id
     echo "------------------------"
