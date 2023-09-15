0c06901205b695dc9bd9e83a29640542d16f2c1c
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-15 16:49:39 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -17,7 +17,9 @@ if [ -z $1 ]
   then 
     echo "Running nodes:"
     ps aux | grep subspace-node | grep -v grep | awk 'match($0, /subspace[0-9]|subspace[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}'
-    read -p "node?  " id
+    echo "------------------------"
+    read -p "Node?  " id
+    echo "------------------------"
   else 
     id=$1
   fi
