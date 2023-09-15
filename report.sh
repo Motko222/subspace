2fb2eccadf9d9c4947d69b1ffb42368011ae4ce1
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-15 16:45:43 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -13,7 +13,15 @@ min_conv () {
  echo $out
 }
 
-if [ -z $id ]; then read -p "node ($ssFilter)?  " id; fi
+if [ -z $1 ]
+  then 
+    echo "Running nodes:"
+    ps aux | grep subspace-node | grep -v grep | awk 'match($0, /subspace[0-9]|subspace[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}'
+    read -p "node?  " id
+  else 
+    id=$1
+  fi
+
 source ~/config/subspace.sh $id
 
 name="subspace"$id
