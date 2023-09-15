174329f48ef8d59bb8d4bd237dc0924634cd6b7a
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-15 21:24:53 +0200

    Update log-node.sh

--- log-node.sh
+++ log-node.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-if [ -z $1 ]; then read -p "node ($ssFilter)?  " id; else id=$1; fi
+if [ -z $1 ]
+  then 
+    echo "Running nodes:"
+    ps aux | grep subspace-node | grep -v grep | awk 'match($0, /subspace[0-9]|subspace[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}'
+    echo "------------------------"
+    read -p "Node?  " id
+    echo "------------------------"
+  else 
+    id=$1
+fi
+
 tail -f ~/logs/subspace_node$id.log
