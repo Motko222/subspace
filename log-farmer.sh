19255cbeefd45ae263898f5a2b076fe702bcddca
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-15 21:57:59 +0200

    Update log-farmer.sh

--- log-farmer.sh
+++ log-farmer.sh
@@ -3,7 +3,7 @@
 if [ -z $1 ]
   then 
     echo "Running farmers:"
-    ps aux | grep subspace-farmer | grep -v grep | awk 'match($0, /subspace[0-9]|subspace[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}'
+    ps aux | grep subspace-farmer | grep -v grep | awk 'match($0, /subspace[0-9]|subspace[0-9][0-9]/) {print substr($0, RSTART, RLENGTH)}'  | sed 's/subspace//g'
     echo "------------------------"
     read -p "Farmer?  " id
     echo "------------------------"
