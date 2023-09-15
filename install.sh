b5b5564e24967ddbe24861247fc3b3581c141d73
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-15 22:31:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,9 +17,9 @@ case $c in
     cd $ssexec
     rm subspace-node* subspace-farmer*
     echo "Get links from here: https://docs.subspace.network/docs/protocol/substrate-cli"
-    then read -p "Node URL? " url
+    read -p "Node URL? " url
     wget $url
-    then read -p "Farmer URL? " url
+    read -p "Farmer URL? " url
     wget $url
     node=$(ls | grep subspace-node)
     farmer=$(ls | grep subspace-farmer)
